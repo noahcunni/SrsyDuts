6edfa5c5fffc8b5138226b19cabf8a985e9ddcd2
Noah Cunningham <noah.cunninghams02@gmail.com>
2026-05-15 09:30:09 -0400

    initial commit of existing code

---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+
+function Card() {
+    
+    return(
+    <div className="card">
+        <img className='cardImage' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdN7FlZR9ZpY_EETzEq4f38wgvGxhATfDfsA&s" alt="profile pic"></img>
+        <h2 className='cardTitle'>NJC</h2>
+        <p>srsyduts srs card webapp</p>
+    </div> 
+    );
+}
+
+export default Card
